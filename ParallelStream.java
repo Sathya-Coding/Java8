ea96b473475aa27551b6c74ad0c760f3c7297735
Sathya <psathyapriya2002@gmail.com>
2025-09-17 11:12:58 +0530

    Add files via upload

---
+++ ParallelStream.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ParallelStream {
+    public static void main(String[]args){
+        List<Integer>numbers= Arrays.asList(1,2,3,4,5,6);
+        numbers.stream().forEach(System.out::println);
+    }
+}
